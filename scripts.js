c58c6846e6a0d63d4f6b8d1c5b60970e0b9d6a5b
Yash Tyagi <y.tyagi@tcu.edu>
2022-10-05 16:51:01 -0500

    Added JS Code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
-    // We are using an initMap callback function, 
-    // i.e. JavaScript code only runs once API has finished loading.
-function initMap(){
-    
-}
+// We are using an initMap callback function, 
+// i.e. JavaScript code only runs once API has finished loading.
+var map;
+var mapMarker;
+function initMap() {
+    latitude  = 34.667752404057154
+    longitude = 135.4305229640065
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: latitude, lng: longitude }, //Latitude and Longitute
+      zoom: 18, //Zoom level
+      mapId: "7f77d77487053fdf"
+    });
+
+    mapMarker = new google.maps.Marker({
+        position: {lat: 34.667752404057154, lng: 135.4305229640065},
+        map,
+        title: "Hello World!",
+        // icon: {
+        //     url: "star.png",
+        //     scaledSize: 
+        // }
+      });
+  }
+
+//   function generateRandomPos(pos) {
+//     return Math.floor(Math.random() * (pos + 2 - pos)) + pos;
